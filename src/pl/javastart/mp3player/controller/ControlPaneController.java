fd1d3e8e6b5b9c267648ccf3cf1119b2fccf2c25
beegitcode <mmc.makowski@gmail.com>
2017-12-27 17:09:44 +0100

    Added method configureSliders in ControlPaneController.class

--- src/pl/javastart/mp3player/controller/ControlPaneController.java
+++ src/pl/javastart/mp3player/controller/ControlPaneController.java
@@ -1,8 +1,10 @@
 package pl.javastart.mp3player.controller;
- 
+
 import java.net.URL;
 import java.util.ResourceBundle;
- 
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -10,57 +12,60 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.control.ToggleButton;
-import javafx.scene.input.MouseEvent;
- 
-public class ControlPaneController implements Initializable{
- 
-    @FXML
-    private Button nextButton;
- 
-    @FXML
-    private Slider volumeSlider;
- 
-    @FXML
-    private Slider songSlider;
- 
-    @FXML
-    private Button prevButton;
- 
-    @FXML
-    private ToggleButton playButton;
- 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        configureButtons();
-        configureVolume();
-    }
- 
-    private void configureVolume() {
-        volumeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                System.out.println("Wciœniêto przycisk na suwaku g³oœnoœci");
-            }
-        });
-    }
- 
-    private void configureButtons() {
-        prevButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Poprzednia piosenka");
-            }
-        });
-        nextButton.setOnAction(x -> System.out.println("Nastêpna piosenka"));
-        playButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                if(playButton.isSelected()) {
-                    System.out.println("Play");
-                } else {
-                    System.out.println("Stop");
-                }
-            }
-        });
-    }
+
+public class ControlPaneController implements Initializable {
+
+	@FXML
+	private Button nextButton;
+
+	@FXML
+	private Slider volumeSlider;
+
+	@FXML
+	private Slider songSlider;
+
+	@FXML
+	private Button prevButton;
+
+	@FXML
+	private ToggleButton playButton;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		configureButtons();
+		// configureVolume();
+		configureSliders();
+	}
+
+	private void configureSliders() {
+		volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+
+				System.out.println("Zmiana g³oœnoœci " + newValue.doubleValue());
+			}
+		});
+
+		songSlider.valueProperty().addListener(x -> System.out.println("Przesuniêcie piosenki"));
+	}
+
+	private void configureButtons() {
+		prevButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Poprzednia piosenka");
+			}
+		});
+		nextButton.setOnAction(x -> System.out.println("Nastêpna piosenka"));
+		playButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				if (playButton.isSelected()) {
+					System.out.println("Play");
+				} else {
+					System.out.println("Stop");
+				}
+			}
+		});
+	}
 }
